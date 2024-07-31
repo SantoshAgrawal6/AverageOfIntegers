d61313e717bce6dd26f016272b73644d03ce28e7
SantoshAgrawal6 <22104043@mail.jiit.ac.in>
2024-07-31 09:58:59 +0530

    Update average.c

--- average.c
+++ average.c
@@ -5,23 +5,19 @@ int main() {
     int sum = 0;
     float average;
 
-    // Read the number of elements
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
     int arr[n];
 
-    // Read the elements
     printf("Enter %d integers:\n", n);
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
         sum += arr[i];
     }
 
-    // Calculate the average
     average = (float)sum / n;
 
-    // Print the result
     printf("The average is: %.2f\n", average);
 
     return 0;
